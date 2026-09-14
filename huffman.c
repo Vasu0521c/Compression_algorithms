050b6502f541ab094784250a13d92790901175ee
Vasu0521c <vasudevanoffline@hotmail.com>
2026-09-14 16:34:45 +0530

    ready for Tree construction.

--- huffman.c
+++ huffman.c
@@ -8,17 +8,39 @@ typedef struct Node Node;
 
 struct Node {
 
-    char *value;
-    int   weight;
+    char  value;
+    int   l_val, r_val;
     Node *left, *right;
 };
 
+Node *new_node(void) {
+
+    Node *newnode    = malloc(sizeof(Node));
+    newnode -> value = '\0';
+    newnode -> l_val = 0;
+    newnode -> r_val = 0;
+    newnode -> left  = NULL;
+    newnode -> right = NULL;
+    return newnode;
+}
+
+Node* assing_children(int *arr, Node *root) {
+
+    Node *temp    = root;
+    temp -> left  = new_node();
+    temp -> right = new_node();
+    temp -> l_val = 0;
+    temp -> r_val = 1;
+    temp -> left -> value  = arr[0];
+    temp -> right -> value = arr[1];
+    return root;
+}
 
 void get_min_vals(int *result, int *arr, int size) {
 
     int i, j, skip, low;
-    i = 0;
-    j = 0;
+    i    = 0;
+    j    = 0;
     skip = -1;
 
     while (i < size - 1) {
@@ -29,12 +51,12 @@ void get_min_vals(int *result, int *arr, int size) {
         }
 
         if (arr[i] < arr[i + 1]) {
-            low = i;
+            low  = i;
             skip = i;
         }
 
         if (i == size - 1) {
-            result[j]  = low;
+            result[j] = low;
             j++;
             if (j > 1)
                 break;
@@ -45,7 +67,7 @@ void get_min_vals(int *result, int *arr, int size) {
 }
 
 int get_value(char *arr, char target) {
-    
+
     int i = 0;
 
     while (arr[i] != '\0') {
@@ -58,7 +80,7 @@ int get_value(char *arr, char target) {
     return -1;
 }
 
-void get_unique_vals(char* input, int *vals, char *chars) {
+void get_unique_vals(char *input, int *vals, char *chars) {
 
     int index, length;
     length = 0;
@@ -120,13 +142,13 @@ int main(void) {
     int   size;
     char *input, *chars;
     int  *vals;
-    int *min;
+    int  *min;
 
-    input       = file_process();
-    size        = get_unique_size(input);
-    min         = malloc(sizeof(int) * 2);
-    vals        = malloc(sizeof(int) * size);
-    chars       = malloc(size + 1);
+    input = file_process();
+    size  = get_unique_size(input);
+    min   = malloc(sizeof(int) * 2);
+    vals  = malloc(sizeof(int) * size);
+    chars = malloc(size + 1);
     memset(vals, 0, size);
     memset(chars, '\0', size);
     chars[size] = '\0';
@@ -134,5 +156,8 @@ int main(void) {
     get_unique_vals(input, vals, chars);
 
     free(input);
+    free(chars);
+    free(vals);
+    free(min);
     return 0;
 }
